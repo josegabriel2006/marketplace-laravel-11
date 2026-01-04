3e94b1a0844e26f1a6ca81f131b463ca861fdd42
josegabriel2006 <zegabrielandrade@gmail.com>
2026-01-04 15:47:40 -0300

    Painel Administrativo - Reconfigurado (Dashboard)

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Nexus Pro</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">NXP</a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Painel</li>
+            <li class="dropdown active">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Configurações</span></a>
+              <ul class="dropdown-menu">
+                <li class=active><a class="nav-link" href="index-0.html">Painel Geral</a></li>
+                <li><a class="nav-link" href="index.html">Painel Ecommerce</a></li>
+              </ul>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+              </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+
+
+        </ul>
+
+       </aside>
+      </div>
