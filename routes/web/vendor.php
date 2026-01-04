1b61a64063939fb412820ee361afa2c2b1f5c334
josegabriel2006 <zegabrielandrade@gmail.com>
2026-01-04 13:54:03 -0300

    first commit

---
+++ routes/web/vendor.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Backend\VendorController;
+use Illuminate\Support\Facades\Route;
+
+// Rota Vendedor
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])
+->middleware(['auth', 'vendor'])
+->name('vendor.dashboard');
+
